f55e82bbe5cb8b8147a51877f3a13e8af398ba1c
Chris Breaux <christopherjbreaux@gmail.com>
2019-01-23 22:29:08 -0800

    create arrays of checked values for use in evaluation conditionals

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
 $(document).ready(function(){
   $("form#stress_test").submit(function(event){
     event.preventDefault();
+    var warningSigns = []
     $("input:checkbox[name=warning-signs]:checked").each(function(){
       var userWarningSigns = $(this).val();
-      console.log(userWarningSigns);
+      warningSigns.push(userWarningSigns);
+
     });
+    var healthConditions = []
     $("input:checkbox[name=health-conditions]:checked").each(function(){
       var userHealthConditions = $(this).val();
-      console.log(userHealthConditions);
+      healthConditions.push(userHealthConditions);
+
     });
+    var relaxTechniques = []
     $("input:checkbox[name=relax-techniques]:checked").each(function(){
       var userRelaxTechniques = $(this).val();
-      console.log(userRelaxTechniques);
+      relaxTechniques.push(userRelaxTechniques);
+
     });
+console.log(warningSigns);
+console.log(healthConditions);
+console.log(relaxTechniques);
+    // the idea is to put the values into separate arrays then compare the lengths of the arrays to use for evaluating conditionals
   });
 });
